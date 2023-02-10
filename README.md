70244a42b5390dcd2b88e0f1e9c3f03ae096dd17
Gurkirat Singh <gsingh8@ualberta.ca>
2023-02-10 16:31:26 -0700

    completed the assignment and licensing

--- README.md
+++ README.md
@@ -11,10 +11,18 @@ Contributors / Licensing
 ========================
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle, 
-https://github.com/tywtyw2002, and https://github.com/treedust
+https://github.com/tywtyw2002, https://github.com/treedust and Gurkirat Singh
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Collaborations with fellow students:
+* Gurveer Singh Sohal
+
+
+Online sources citing:
+* mozilla.org, 2023, https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET
+* mozilla.org, 2023, https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+* Mazen Aly, 2015, https://stackoverflow.com/questions/5552555/unicodedecodeerror-invalid-continuation-byte
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2023 Abram Hindle, https://github.com/tywtyw2002, https://github.com/treedust and Gurkirat Singh
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+import urllib.parse 
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -51,7 +51,7 @@ class HTTPClient(object):
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -65,17 +65,98 @@ class HTTPClient(object):
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode('latin1')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # print("New test case !!: ", url)
+        o = urllib.parse.urlparse(url)
+        # print(o)
+        host = o.hostname
+        if o.port == None:
+            port = 80
+        else:
+            port = o.port
+
+        request = 'GET /'+o.path+' HTTP/1.1\r\nHost: '+o.hostname+'\r\n\r\nConnection: close\r\n\r\n'
+
+        self.connect(host, port)
+        self.sendall(request)
+
+        ret = self.recvall(self.socket)
+        self.close()
+
+        elements = str(ret.strip('\r\n')).split('\r\n')
+        body = elements[len(elements)-1]
+        code = elements[0].split()[1]
+        # print(code)
+
+        return HTTPResponse(int(code), body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        #making form data
+        form_data = ''
+        if args != None:
+            # form_data = ""
+            # keys = list(args)
+            # # print(keys[0])
+            # for key_in in range(len(keys)):
+            # # for key in keys:
+            #     key_split = keys[key_in].split()
+            #     #store field name
+            #     for i in range(len(key_split)):
+            #         if i == len(key_split) - 1:
+            #             form_data += key_split[i]+"="
+            #         else:
+            #             form_data += key_split[i]+"+"
+
+            #     #store value entry
+            #     value = args.get(keys[key_in])
+            #     value_split = value.split()
+            #     for i in range(len(value_split)):
+            #         if (i == len(value_split)-1 ) and (key_in == len(keys)-1 ):
+            #             form_data += value_split[i]
+            #         elif (i != len(value_split)-1 ):
+            #             form_data += value_split[i]+"+"
+            #         else:#i == len(value_split)-1
+            #             form_data += value_split[i]+"&"
+            
+            # # print(form_data)
+            # length = str(len(form_data))
+            # print(form_data)
+
+            keys = list(args)
+            for key in keys:
+                form_data+=key+'='
+                value = args.get(key)
+                form_data+=value+'&'
+            form_data.strip('&')
+            length = str(len(form_data))
+
+
+        else:   # if args == None
+            form_data = ''
+            length = '0'
+
+        o = urllib.parse.urlparse(url)
+        host = o.hostname
+        if o.port == None:
+            port = 80
+        else:
+            port = o.port
+        
+        self.connect(host, port)
+
+        request = "POST /test HTTP/1.1\r\nHost: "+o.hostname+"\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: "+length+"\r\n\r\n"+form_data+"\r\n\r\n"
+
+        self.sendall(request)
+        ret = self.recvall(self.socket)
+        self.close()
+
+        elements = str(ret.strip('\r\n')).split('\r\n')
+        body = elements[len(elements)-1]
+        code = elements[0].split()[1]
+        # body = ""
+        return HTTPResponse(int(code), body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
@@ -90,6 +171,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command( sys.argv[2], sys.argv[1]))
     else:
         print(client.command( sys.argv[1] ))
